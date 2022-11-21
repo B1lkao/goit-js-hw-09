0536464e1b9acc6a6aa55051faefda3bd13436e7
B1lkao <hrabousolesia@gmail.com>
2022-11-21 14:04:08 +0200

    Виконала перше завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const off= document.querySelector('[data-stop]');
+let timerId = null;
+
+const onStartClick = () => {
+    start.setAttribute('disabled', null);
+    timerId = setInterval(()=>{
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+const onOffClick=()=>{
+    clearInterval(timerId);
+    start.removeAttribute('disabled');
+}
+
+start.addEventListener('click', onStartClick);
+off.addEventListener('click', onOffClick);
